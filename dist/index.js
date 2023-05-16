77b9d2f4f80394aebd2e568d7c656c9688185a7f
whl <41020733@qq.com>
2023-05-16 13:44:23 +0800

    fix: add new feature

--- dist/index.js
+++ dist/index.js
@@ -9734,6 +9734,11 @@ function run() {
     const repository = core.getInput('repo');
     const evt = core.getInput('evt');
     const startTime = core.getInput('startTime');
+    core.debug("evt:" + evt);
+    core.debug("startTime:" + startTime);
+    // 秒时间戳
+    const startAt = Math.floor(Date.now() / 1000);
+    core.setOutput('startAt', startAt.toString());
     const msg = new Robot(dingToken, getActionCard({
         runId: runId,
         ref: ref,
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,13 @@ function run() {
     const evt = core.getInput('evt')
     const startTime = core.getInput('startTime')
 
+    core.debug("evt:"+evt);
+    core.debug("startTime:"+startTime);
+
+    // 秒时间戳
+    const startAt = Math.floor(Date.now() / 1000);
+    core.setOutput('startAt',startAt.toString())
+
     const msg = new Robot(dingToken, getActionCard({
         runId: runId,
         ref: ref,
